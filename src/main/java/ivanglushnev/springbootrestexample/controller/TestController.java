71a9f3cf1ddf68d05db1b6a9540133281ce9ce54
IvanGlushnev <ivan.glushnev@leroymerlin.ru>
2024-03-02 13:05:56 +0300

    add test controller

--- src/main/java/ivanglushnev/springbootrestexample/controller/TestController.java
+++ src/main/java/ivanglushnev/springbootrestexample/controller/TestController.java
@@ -1,2 +1,16 @@
-package ivanglushnev.springbootrestexample.controller;public class TestController {
+package ivanglushnev.springbootrestexample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+  @GetMapping
+  public String helloWorld() {
+    return "HelloWorld";
+  }
+
 }
