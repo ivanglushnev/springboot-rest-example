7b739f661dcff915154429d12fbbbe1b301326dd
IvanGlushnev <ivan.glushnev@leroymerlin.ru>
2024-03-02 12:59:53 +0300

    init commit

---
+++ src/test/java/ivanglushnev/springbootrestexample/SpringbootRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ivanglushnev.springbootrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
